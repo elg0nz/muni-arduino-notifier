2fb0f66deac046e72c3c734e8db12dda74e5769e
elg0nz <elg0nz@gmail.com>
2014-04-01 21:49:13 -0700

    Update the client to iterate over each channel

--- nde-banner/websocket_client.js
+++ nde-banner/websocket_client.js
@@ -1,11 +1,27 @@
 var io   = require('socket.io-client')
   , http = require('http')
+  , util = require('util')
   , host = 'http://localhost:8080';
 
-var get_subscriptions = function(data){
-  console.log(data);
-  http.get(host, function (res) {
-    res.on('data', function (chunk) { console.log('' + chunk); });
+var get_subscriptions = function (channels){
+  channels.forEach(process_channel);
+};
+
+var display_response = function (channel, chunk) {
+  var data = JSON.parse(chunk);
+  var msg = data["msg"];
+  var channel = data["channel"];
+
+  var output = util.format('%s: %s', channel, msg);
+  console.log(output);
+};
+
+var process_channel = function (channel) {
+  var channel_path = util.format('%s/%s', host, channel);
+  http.get(channel_path, function (res) {
+    res.on('data', function (chunk) {
+        display_response(channel, chunk);
+    });
   }).on('error', function (e) {
     console.log("Got error: " + e.message);
   });
